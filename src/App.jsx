a6f19312458ebbf9e7b285d85f76c4c8632f818b
RConradGe <rcgadduang@gmail.com>
2025-03-27 14:18:27 -0700

    wip: added weatherService service and WeatherSearch component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 // src/App.jsx
 import { useState } from 'react';
+import * as weatherService from './services/weatherService';
+import WeatherSearch from './components/WeatherSearch/WeatherSearch'
 import './App.css';
 
 
 const App = () => {
-
+  const fetchData = async () => {
+    const data = await weatherService.show('Fort Worth');
+    const newWeatherState = {
+      location: data.location.name,
+      temperature: data.current.temp_f,
+      condition: data.current.condition.text
+    }
+  };
   return (
-    <h1>Hello world!</h1>
+    <main>
+      <h1>Weather API</h1>
+      <WeatherSearch />
+      <button onClick={fetchData}>Fetch Weather Data</button>
+    </main>
   );
 }
 
--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -1,15 +1,17 @@
-const API_KEY = '';
+const API_KEY = '83f3f43492cf4f33bb4184223252703';
 const BASE_URL = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}`;
 
 const show = async (city) => {
   try {
-      const queryString = `& q=${city}`;
-      const res = await fetch(BASE_URL + queryString);
-      const data = await res.json()
-      return data;
-  } catch (error) {
-      console.error(error);
+    const queryString = `&q=${city}`;
+    const res = await fetch(BASE_URL + queryString);
+    const data = await res.json();
+    console.log('Data:', data);
+    return data;
+  } catch (err) {
+    console.log(err);
   }
 };
+
 
 export { show }; // "named export syntax"
